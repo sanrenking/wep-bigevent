88e70048771407fb329fa8e57cf7424627903547
yihe <970384872@qq.com>
2020-11-19 23:13:51 +0800

    login.css样式修改 login.js 事件注册  login.html 框架修改

--- day-01/code/assets/js/login.js
+++ day-01/code/assets/js/login.js
@@ -1,13 +1,14 @@
 $(function () { 
+  //点击注册界面的注册事件
 $('#link_reg').on('click',function () {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
-
+//点击登录界面的注册事件
   $('#link_login').on('click',function () {
-$('.reg-box').hide()
-$('.login-box').show()
+    $('.reg-box').hide()
+    $('.login-box').show()
     })
 
 
