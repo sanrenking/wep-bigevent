2010bff9830cda15ac2ff685a64e54b767e064a1
yihe <970384872@qq.com>
2020-11-19 19:26:02 +0800

    新添加login.js login.css 文档  初始化 页面样式

---
+++ day-01/code/assets/js/login.js
@@ -0,0 +1,15 @@
+$(function () { 
+$('#link_reg').on('click',function () {
+    $('.login-box').hide()
+    $('.reg-box').show()
+  })
+
+
+  $('#link_login').on('click',function () {
+$('.reg-box').hide()
+$('.login-box').show()
+    })
+
+
+    
+ })
